75b3f676e246581103568ba3b19d74424c1338f5
miiicom <miiicom1230@gmail.com>
2019-01-11 14:53:20 -0700

    Add parent

--- scripts/01.py
+++ scripts/01.py
@@ -1,5 +1,6 @@
-import maya.cmds as cmds
 import pymel.core as pm
+
+
 def createLocator(name, x,y,z):
 	locator = pm.spaceLocator(n = name)
 	locator.t.set(x,y,z)
@@ -13,17 +14,24 @@ def createAndBindJointToLoc(jointName, LocHandler):
 	return jointHandler
 
 #create 5 locator
+JOINT_NUMBER = 5
 locatorGroup = []
-for loopNum in range(5):
+for loopNum in range(JOINT_NUMBER):
 	locatorName = 'LegLocator' + str(loopNum)
 	locatorGroup.append (createLocator(locatorName, 0, loopNum * 5, 0))
 	pm.select(d=True)
 
 legLocatorGroup = pm.group(locatorGroup, n='legLocatorGroup')
+#should have a UI to let artist use locator around
 
-
-for loopNum in range(5):
+# create 5 joint and snap them to the locator
+jointGroup = []
+for loopNum in range(JOINT_NUMBER):
 	pm.select(d=True)
 	jointName = 'legJoint' + str(loopNum)
-	createAndBindJointToLoc?jointName,locatorGroup[loopNum])
+	jointGroup.append(createAndBindJointToLoc(jointName,locatorGroup[loopNum]))
 
+print(jointGroup)
+# parent those joints
+for loopNum in range(0,JOINT_NUMBER-1):
+	pm.parent(jointGroup[loopNum],jointGroup[loopNum+1])
