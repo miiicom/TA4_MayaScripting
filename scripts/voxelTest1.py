a070fe0c2c120ab52fc0a038d7d97f4f21e5df09
miiicom <miiicom1230@gmail.com>
2019-02-15 14:18:30 -0700

    Fix indent bug, add calculate average edge length function

--- scripts/voxelTest1.py
+++ scripts/voxelTest1.py
@@ -7,6 +7,8 @@ class myWindowClass:
 	title = 'defaultTitle';
 	size = (512,512);
 	
+	sampleRate = 1; #Default to 1
+	
 	def _init_(self,name):
 		self.windowName = 'myUIWindow';
 		self.title = name;
@@ -27,7 +29,6 @@ class myWindowClass:
 		return(vertexGrp);
 	
 	def getMeshFaces(self,mesh):
-
 		FaceGrp = [];
 		print(cmds.polyEvaluate(mesh, f = True));
 		for x in range(cmds.polyEvaluate(mesh, f = True)):
@@ -42,9 +43,34 @@ class myWindowClass:
 			EdgeGrp.append([mesh+".e[%s]"%x])
 		return(EdgeGrp);
 		
+	def calculateDistanceBetweenVerts(v1, v2):
+		print();
+		Location1 = cmds.xform(v1, q=True, t=True);
+		Location2 = cmds.xform(v2, q=True, t=True);
+    	
+		return math.sqrt((Location2[0]-Location1[0])**2 + (Location2[1]-Location1[1])**2 + (Location2[2]-Location1[2])**2);
+    	
+	def calculateAvgEdgeLRN(self,mesh):
+		
+		edgeGrp = self.getMeshEdges(mesh);
+		average = 0;
+		count = 1;
+		
+		for x in range(int(len(edgeGrp) * self.sampleRate)):
+			sampleEdge = random.choice(edgeGrp);
+			edgeGrp.remove(sampleEdge);
+			cmds.select(sampleEdge);
+			cmds.ConvertSelectionToVertices();
+			sampleVerts = cmds.ls(sl=True, fl=True);
+			distance = calculateDistanceBetweenVerts(sampleVerts[0],sampleVerts[1]);
+			average = ((count-1)*average + distance)/count
+			count += 1;
+		return average;
+		
+		
 testWindow = myWindowClass();
 testWindow._init_('Material It Now!');
 testWindow.create();
 	
 selected = cmds.ls(sl=True)[0];
-print(testWindow.getMeshEdges(selected));
+print(testWindow.calculateAvgEdgeLRN(selected));
